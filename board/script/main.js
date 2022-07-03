5c34ccb349153220d15730b3a2c4c2389c2d7843
Yeonwoo Ga <vjk4874@naver.com>
2022-07-03 18:17:00 +0900

    0703 리스트만들기

---
+++ board/script/main.js
@@ -0,0 +1,7 @@
+const init = () =>{
+    $('header').load('/board/fragment/header.html');
+    $('nav').load('/board/fragment/nav.html');
+    $('aside').load('/board/fragment/aside.html');
+    $('footer').load('/board/fragment/footer.html');
+
+}
